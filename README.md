c2d4be7ea2a6b470c853ca201c2eb9620056922f
sachin kumar gautam <sachinbvp100@gmailcom>
2018-09-19 13:39:46 +0530

    Yay its working

--- README.md
+++ README.md
@@ -4,4 +4,6 @@ This boilerplate is Using React, Redux, Redux-observables, Webpack, babel,
 
 Currently this boilerplate is using React-toolkit as ui library will be updated in Material ui in future 
 
+This Projects supports Redux, Redux-observables out of the box.
+
 
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,14 @@
 import React, { Component } from 'react'
 class App extends Component {
   render () {
-    return <div>Sachin is awesome reatc enginner</div>
+    return (
+      <div>
+        Welcome to React bolierplate
+        <p>
+          Make sure to do npm install
+        </p>
+      </div>
+    )
   }
 }
 
