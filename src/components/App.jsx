5ee6770648e2b2b07c36ec23b2f3077d1ad8b530
sachin kumar gautam <sachinbvp100@gmailcom>
2018-10-20 14:00:25 +0530

    deleted files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,10 @@
 import React, { Component } from 'react'
-import Main from './main'
-import Header from './header'
-import Footer from './footer'
 import withRoot from '../withRoot.js'
 class App extends Component {
   render () {
     return (
       <div>
-        <Header />
-        <Main />
-        <Footer />
+        This is the index.js{' '}
       </div>
     )
   }
